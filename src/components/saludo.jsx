89e91b3aa298b9328199942b40132b4dcda5db5e
Rasmeh27 <luisgnacio123@gmail.com>
2025-02-08 23:39:06 -0400

    first commit

---
+++ src/components/saludo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Vamos a utilizar tailwindcss para darle estilo a nuestro componente
+const Saludo = () => {
+    return (
+      <div className="text-center text-2xl font-bold text-blue-500">
+        ¡Hola, mundo!
+      </div>
+    );
+  };
+  
+  export default Saludo;
