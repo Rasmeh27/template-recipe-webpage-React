3781068344c9d8d36b611c891c02410baf5474f8
Rasmeh27 <luisgnacio123@gmail.com>
2025-02-10 17:12:02 -0400

    Header (incomplete II)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,31 @@
 import React from "react"
+import { FaSearch, FaBars, FaUserCircle } from "react-icons/fa";
 
+/*Learning React Js and TailwindCSS: This is a component for to watch the header with his options */
 function Header() {
     return (
-      <header className="bg-white p-4">
-        <nav className="flex items-center justify-between">
-            <div>
-                <h1 className="text-black">RecipeWebPage</h1>
-            </div>
-        <div>
-            <input type="text" id="text" placeholder="search..." className="bg-input pl-3 p-3 text-gray-700" />
-            <button className="bg-green-400 text-white p-3">Search</button> 
-        </div>
-            <ul className="flex items-center justify-between">
-                <li className="ml-4 text-green-400"><a href="#home">Home</a></li>
-                <li className="ml-4 text-black"><a href="#home">Explore</a></li>
-                <li className="ml-4 text-black"><a href="#home">Help</a></li>
-            </ul>
-        </nav>
+        <header className="bg-white p-4 shadow-md">
+            <nav className="flex items-center justify-between">
+                {/* Logo y menú */}
+                <div className="flex items-center justify-between">
+                <FaBars className="text-gray-700 text-2xl mr-3 cursor-pointer" />  
+                    <h1 className="text-black text-xl font-bold">Logo-RecipeWebPage</h1>
+                </div>
+                {/* Search */}
+                <div className="flex items-center border-gray-300 rounded-lg overflow-hidden">
+                    <select className="bg-gray-100 p-3 border-gray-300 text-sm text-black outline-none cursor-pointer" id="category">
+                        <option>All Categories</option>
+                    </select>
+                    <input type="text" id="text" placeholder="search..." className="bg-gray-100 p-2 text-gray-700 outline-none"/>
+                    <button className="bg-green-500 p-3 px-3 text-white cursor-pointer"><FaSearch></FaSearch></button>
+                </div>
+                <ul className="flex items-center justify-between">
+                    <li className="ml-4 text-green-400"><a href="#home">Home</a></li>
+                    <li className="ml-4 text-black"><a href="#explore">Explore</a></li>
+                    <li className="ml-4 text-black"><a href="#help">Help</a></li>
+                    <li><FaUserCircle className="ml-4 text-green-500 text-3xl cursor-pointer"></FaUserCircle></li>
+                </ul>
+            </nav>
         </header>
     );
 }
